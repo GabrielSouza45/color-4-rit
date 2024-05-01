c71e70698a364e2a6142546efbc962e9a2544851
AntonioJsNeto <ns4809623@gmail.com>
2024-04-30 23:30:19 -0300

    Criação do test e atualização do get

--- src/main/webapp/js/ritmo/api/getPlacar.js
+++ src/main/webapp/js/ritmo/api/getPlacar.js
@@ -1,13 +1,41 @@
 import {requestJson} from "../service/requestPost.js";
 import Placar from "../model/Placar.js";
 
+const element = document.getElementById("acessar-placar"); 
+element.addEventListener("click", getPlacar);
+
 export function getPlacar (idMapa, idJogador){
     const placarJson = new Placar(0, 0, null, null, idMapa, idJogador);
     const json = JSON.stringify(placarJson);
     const requestOptions = requestJson (json);
 
-    fetch("/get-placar", requestOptions)
+    return fetch("/get-placar", requestOptions)
     .then((response) => {
-        if(!response.ok)
+        if(!response.ok && !response.status === 404){
+           throw new Error("Erro ao coletar dados do placar!");
+        }else if(response.status === 404){
+            throw new Error("Não foram encontrados os dados do placar!");
+        }
+        return response.json();
+    })
+    
+    .then((placar) => {
+        
+        console.log(placar);
+
+        return new Placar(
+            placar.id,
+            placar.pontuacao,
+            placar.jogador,
+            placar.mapa,
+            placar.idMapa,
+            placar.idJogador
+        );
+    })
+
+    .catch((erro) =>{
+        alert("Erro ao pegar o placar no Backend");
+        console.error(erro);
+        throw new Error(erro);
     })
 }
