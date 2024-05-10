2eb89986b2dd17be5ef1b4a9e79c25341f18a222
Marco Mafra <mamengo2012@gmail.com>
2024-05-10 20:47:59 -0300

    att conectarDAO

--- src/main/java/br/com/color4rit/controller/ConectarDao.java
+++ src/main/java/br/com/color4rit/controller/ConectarDao.java
@@ -5,8 +5,8 @@ import java.sql.*;
 public class ConectarDao {
     private String driver = "com.mysql.cj.jdbc.Driver";
     private final static String url =
-            "jdbc:mysql://color4rit.ctqs8sckwdnb.sa-east-1.rds.amazonaws.com:3306/color4rit?useTimeZone=true&serverTimeZone=UTC";
-    private final static String usuario = "root";
+            "jdbc:mysql://color4rit.cpgiqwu4wvyq.us-east-1.rds.amazonaws.com:3306/color4rit?useTimeZone=true&serverTimeZone=UTC";
+    private final static String usuario = "admin";
     private final static String senha = "color#123";
     private static Connection conexao;
 
