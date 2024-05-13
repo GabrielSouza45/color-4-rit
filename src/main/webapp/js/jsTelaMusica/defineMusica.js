78e8bab00287c897137ed4997aae14a71285889a
Gabriel Freitas Souza <gabrielsouza45@live.com>
2024-05-12 22:54:09 -0300

    att

--- src/main/webapp/js/jsTelaMusica/defineMusica.js
+++ src/main/webapp/js/jsTelaMusica/defineMusica.js
@@ -1,27 +1,30 @@
 import { getMusica } from "./getMusica.js";
 
-const element = document.getElementById("botaoContinuar");
-element.addEventListener("click", defineMusica);
+window.onload = defineMusica();
 
+async function defineMusica() {
+  const selectMusica = document.getElementById("selectMusica");
+  let listaDeMusicas = [];
 
-async function defineMusica (){
-    const selectMusica = document.getElementById("selectMusica");    
-    let listaDeMusicas = [];
+  await getMusica()
+    .then((musicas) => {
+      listaDeMusicas = musicas;
+    })
 
-    await getMusica()
-        .then((musicas)=>{
-            listaDeMusicas = musicas;
-        })
+    .catch((error) => {
+      throw new Error(error);
+    });
 
-        .catch((error) =>{
-            throw new Error(error);
-        });
+  selectMusica.innerHTML = "";
+  const option1 = document.createElement("option");
+  option1.value = 0;
+  option1.textContent = "Selecione uma música.";
+  selectMusica.appendChild(option1);
 
-        selectMusica.innerHTML = "";
-        listaDeMusicas.forEach(musica=>{
-            const option = document.createElement("option");
-            option.value = musica.id;
-            option.textContent = musica.nome;
-            selectMusica.appendChild(option);
-        })
+  listaDeMusicas.forEach((musica) => {
+    const option = document.createElement("option");
+    option.value = musica.id;
+    option.textContent = musica.nome;
+    selectMusica.appendChild(option);
+  });
 }
