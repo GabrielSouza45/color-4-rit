ea754682f3d00e95f7144c845d663581de089aa8
Gabriel Freitas Souza <gabrielsouza45@live.com>
2024-05-19 16:15:40 -0300

    Update ConectarDao.java

--- src/main/java/br/com/color4rit/controller/ConectarDao.java
+++ src/main/java/br/com/color4rit/controller/ConectarDao.java
@@ -4,10 +4,20 @@ import java.sql.*;
 
 public class ConectarDao {
     private String driver = "com.mysql.cj.jdbc.Driver";
-    private final static String url =
+
+
+    private final static String urlLocal =
+            "jdbc:mysql://localhost:3306/color4rit";
+    private final static String usuarioLocal = "root";
+    private final static String senhaLocal = ""; // Defina a senha do seu banco local
+
+
+    private final static String urlAws =
             "jdbc:mysql://color4rit.cpgiqwu4wvyq.us-east-1.rds.amazonaws.com:3306/color4rit?useTimeZone=true&serverTimeZone=UTC";
-    private final static String usuario = "admin";
-    private final static String senha = "color#123";
+    private final static String usuarioAws = "admin";
+    private final static String senhaAws = "color#123";
+
+
     private static Connection conexao;
 
 
@@ -16,10 +26,26 @@ public class ConectarDao {
         try {
             if (conexao == null) {
                 Class.forName(driver);
-                conexao = DriverManager.getConnection(url, usuario, senha);
+
+                try {
+                    conexao = DriverManager.getConnection(urlAws, usuarioAws, senhaAws);
+                    System.out.println("Conectado Banco AWS");
+                } catch (Exception e) {
+                    System.out.println("Erro ao conectar na AWS -> " + e.getMessage());
+                }
+
+                if (conexao == null) {
+                    try {
+                        conexao = DriverManager.getConnection(urlLocal, usuarioLocal, senhaLocal);
+                        System.out.println(" \nConectado Banco Local");
+
+                    } catch (Exception e) {
+                        System.out.println(" \nErro ao conectar no BD Local -> " + e.getMessage());
+                    }
+                }
             }
             return conexao;
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
             return null;
         }
